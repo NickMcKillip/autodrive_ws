9db1769699ace3afbace52c7c5b9c8311b858d60
Juan Vasquez <kipvasq9@tamu.edu>
2018-11-09 15:26:49 -0600

    lol works

--- scripts/camera_detection.py
+++ scripts/camera_detection.py
@@ -51,8 +51,9 @@ keras.backend.tensorflow_backend.set_session(get_session())
 
 # adjust this to point to your downloaded/trained model
 # models can be downloaded here: https://github.com/fizyr/keras-retinanet/releases
-model_path = "/home/perception/Desktop/catkin_ws/src/keras-retinanet/snapshots/juan.h5"
+model_path = "/home/autodrive/Desktop/catkin_ws/src/keras-retinanet/snapshots/juan.h5"
 model = models.load_model(model_path, backbone_name='resnet50')
+model._make_predict_function()
 
 # load label to names mapping for visualization purposes
 labels_to_names = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
@@ -71,21 +72,14 @@ def updateImage(image_msg):
 
     image = bridge.imgmsg_to_cv2(image_msg, "bgr8")
     image, scale = resize_image(image)
-    cv2.imwrite("/home/perception/Desktop/catkin_ws/src/keras-retinanet/scripts/image.jpg", image)
-    time.sleep(0.10)
-    
-def retinanet():
-    try:
-        image = read_image_bgr("/home/perception/Desktop/catkin_ws/src/keras-retinanet/scripts/image.jpg")
-    except:
-        return
-
+   
+    retinanet(image)
+ 
+def retinanet(image):
     # convert cv mat to np array through PIL
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     image = PIL.Image.fromarray(image)
     image = np.asarray(image.convert('RGB'))[:, :, ::-1].copy()
-
-    print(image.shape)
 
     # copy to draw on
     draw = image.copy()
@@ -107,7 +101,7 @@ def retinanet():
     # visualize detections
     for box, score, label in zip(boxes[0], scores[0], labels[0]):
         # scores are sorted so we can break
-        if score < 0.5:
+        if score < 0.3:
             break
 
         color = label_color(label)
@@ -117,6 +111,8 @@ def retinanet():
 
         caption = "{} {:.3f}".format(labels_to_names[label], score)
         draw_caption(draw, b, caption)
+
+        print("I see a " + str(labels_to_names[label]) + "(" + str(score) + ")")
 
     plt.figure(figsize=(15, 15))
     plt.axis('off')
@@ -136,10 +132,13 @@ if __name__ == "__main__":
     rospy.init_node('camera_detection_node', anonymous=True)
     sub_image = rospy.Subscriber('/front_camera/image_raw', Image, updateImage, queue_size=1)
 
-    while not rospy.is_shutdown():
-        #image = read_image_bgr("/home/perception/Desktop/Bobby/keras-retinanet/examples/juan.JPG")
-        #image = cv2.imread("/home/perception/Desktop/Bobby/keras-retinanet/examples/juan.JPG")
-        retinanet()
+
+    rospy.spin()
+
+    #while not rospy.is_shutdown():
+    #    #image = read_image_bgr("/home/perception/Desktop/Bobby/keras-retinanet/examples/juan.JPG")
+    #       #image = cv2.imread("/home/perception/Desktop/Bobby/keras-retinanet/examples/juan.JPG")
+    #    retinanet()
 
 
 
@@ -172,7 +171,7 @@ if __name__ == "__main__":
 #         # begin processing images
 #         self.processImages()
 #
-
+#
 #     # ROS subscriber callback
 #     def imageCallback(self, image_msg):
 #         bridge = CvBridge()
